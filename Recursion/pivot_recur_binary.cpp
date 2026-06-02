801eb9c13e73e0c09fca77da3286b7365cb9978b
Jashwanth-Uchiha <pjashwanth5@gmail.com>
2026-06-02 23:19:55 +0530

    recursion series

---
+++ Recursion/pivot_recur_binary.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int pivot(int nums[],int start,int end){
+int mid=start+(end-start)/2;
+if(nums[mid]<nums[0]){
+    if(mid>0 &&nums[mid]<nums[mid-1]){
+        return mid;
+    }
+    end=mid;
+}
+else{
+    start=mid+1;
+}
+return pivot( nums, start, end);
+}
+int main(){
+    int array[]={3,4,5,6,1,2};
+    int start=0;int end=5;
+    int k=pivot(array,start,end);
+    cout<<array[k]<<endl;
+    return 0;
+}
